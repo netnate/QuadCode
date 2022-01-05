0629e6cbf0680da6904f3b18053b0f12570ef854
Ilya Vershinin <78657860+netnate@users.noreply.github.com>
2022-01-05 19:10:08 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:quade_code/pages/home.dart';
+
+
+void main() => runApp(MaterialApp(
+  theme: ThemeData(
+    primaryColor: Colors.indigo,
+  ),
+  home: Home(),
+));
